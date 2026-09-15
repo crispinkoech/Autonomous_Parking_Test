4028c8c9993e81646201952654d87c21772476f3
Crispin Koech <crispinkoech.ck@gmail.com>
2026-09-15 15:12:55 +0200

    Implement and test MoveForward(), MoveBackward() and UnPark()

---
+++ src/test/java/TestMoveBackward.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMoveBackward {
+    private IDataSensor sensor1 = new MockDataSensor(new int[] {101, 100, 102, 104, 105});
+    private IDataSensor sensor2 = new MockDataSensor(new int[] {103, 100, 101, 99, 98});
+
+    @Test
+    void MoveBackwardRejectsInvalidPosition() {
+        AutonomousParking car = new AutonomousParking(sensor1, sensor2);
+        car.currCarPosition = 1;
+
+        Error error = assertThrows(Error.class, () -> car.MoveBackward());
+        assertEquals(error.getMessage(), "Invalid car position");
+
+        car.currCarPosition = 501;
+        error = assertThrows(Error.class, () -> car.MoveBackward());
+        assertEquals(error.getMessage(), "Invalid car position");
+    }
+
+    @Test
+    void MoveBackwardRejectsInvalidNumberOfFreeSpots() {
+        AutonomousParking car = new AutonomousParking(sensor1, sensor2);
+        car.currCarPosition = 2;
+
+        car.freeSpotsLength = -1;
+
+        Error error = assertThrows(Error.class, () -> car.MoveBackward());
+        assertEquals(error.getMessage(), "Invalid free spots length");
+
+        car.freeSpotsLength = 10;
+        error = assertThrows(Error.class, () -> car.MoveBackward());
+        assertEquals(error.getMessage(), "Invalid free spots length");
+    }
+
+    @Test
+    void MoveBackwardDecrementsPositionByOneAndResetFreeSpotsLength() {
+        AutonomousParking car = new AutonomousParking(sensor1, sensor2);
+        car.currCarPosition = 2;
+
+        // Assume we had already detected free 3m before this
+        car.freeSpotsLength = 2;
+
+        FreeSpots freeSpots = assertDoesNotThrow(() -> car.MoveBackward());
+        assertEquals(1, freeSpots.position); // Car position should be 1
+        assertEquals(0, freeSpots.freeSpotsLength); // Detected free spots should be reset.
+    }
+
+    @Test
+    void MoveBackwardDecrementsPositionAndIncrementsFreeSpotsByOne() {
+        AutonomousParking car = new AutonomousParking(
+            new MockDataSensor(new int [] {151, 152, 148, 160, 143}),
+            new MockDataSensor(new int [] {155, 149, 147, 166, 161})
+        );
+        car.currCarPosition = 2;
+
+        // Assume we had already detected free 3m before this
+        car.freeSpotsLength = 2;
+
+        FreeSpots freeSpots = assertDoesNotThrow(() -> car.MoveBackward());
+        assertEquals(1, freeSpots.position); // Car position should be 1
+        assertEquals(3, freeSpots.freeSpotsLength); // Detected free spots should be 3
+    }
+}
---
+++ src/test/java/TestUnpark.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestUnpark {
+    private IDataSensor sensor1 = new MockDataSensor(new int[] {});
+    private IDataSensor sensor2 = new MockDataSensor(new int[] {});
+
+    @Test
+    void UnparkRejectsInvalidPosition() {
+        AutonomousParking car = new AutonomousParking(sensor1, sensor2);
+        car.currCarPosition = 0; // Outside the parking range
+
+        Error error = assertThrows(Error.class, () -> car.UnPark());
+        assertEquals(error.getMessage(), "Invalid car position");
+
+        car.currCarPosition = 501; // Outside the parking range
+        error = assertThrows(Error.class, () -> car.UnPark());
+        assertEquals(error.getMessage(), "Invalid car position");
+    }
+
+    @Test
+    void UnparkDoesNothingWhenCarIsAlreadyUnparked() {
+        AutonomousParking car = new AutonomousParking(sensor1, sensor2);
+        car.currCarPosition = 1; // Keep car within parking range
+
+        assertDoesNotThrow(() -> car.UnPark());
+        assertEquals(1, car.currCarPosition); // Car position should be unchanged
+        assertEquals(ParkingStatus.UNPARKED, car.currParkingStatus); // Car should still be unparked
+    }
+
+    @Test
+    void UnparkChangesCarStateToUnparkedIfCarWasParked() {
+        AutonomousParking car = new AutonomousParking(sensor1, sensor2);
+        car.currCarPosition = 1; // Keep car within parking range
+        car.currParkingStatus = ParkingStatus.PARKED; // Car is parked
+
+        assertDoesNotThrow(() -> car.UnPark());
+        assertEquals(1, car.currCarPosition); // Car position should be unchanged
+        assertEquals(ParkingStatus.UNPARKED, car.currParkingStatus); // Car should still be unparked
+    }
+}
